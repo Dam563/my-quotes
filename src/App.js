92e25d3d9429644097f0a41aec308c128faaa351
Damilola Babatunde <babatundedamilola334@gmail.com>
2024-03-15 14:40:06 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState, useEffect } from 'react';
+// import axios from "axios";
+import data from "./data/quote.json"
+
+const App = () => {
+  // const [data, setData] = useState("");
+  // console.log("data", data);
+
+console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((item) =>(
+        <div>
+     <p>{item.a}</p>
+     <p>{item.c}</p>
+     <p>{item.q}</p>
+     </div>
+    ))}
     </div>
   );
 }
